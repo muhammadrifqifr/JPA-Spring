54c36ebd175c405e95843f32f589222488e6c499
Rifqi Fauzi Ramdhani <46875859+muhammadrifqifr@users.noreply.github.com>
2023-01-16 12:22:24 +0700

    repository updated

--- demo/demo/src/main/java/com/example/demo/repositories/EmployeeRepository.java
+++ demo/demo/src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -10,4 +10,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query
     (value = "SELECT e.id FROM tb_m_employee e WHERE e.email =?1", nativeQuery = true)
     public Integer findIdByEmail(String email);
+
+    @Query
+    (value = "SELECT e.email, u.password FROM tb_m_employee e JOIN tb_m_user u ON e.id = u.id WHERE e.email =?1 && u.password =?2", nativeQuery = true)
+    public String findEmailNPass(String email, String password);
 }
